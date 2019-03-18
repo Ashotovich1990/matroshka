bd6e00a2eb8d9d717c37cd950caf8ce53157f19f
Aram Sargsyan <aramsargsyan9902gmail.com>
2019-03-17 20:30:47 -0700

    Fix a type in the intro

--- src/bundle.js
+++ src/bundle.js
@@ -836,7 +836,7 @@ class Intro {
         this.ctx.fillStyle = "white";
         this.ctx.textAlign = "center";
         this.ctx.fillText('Rules Matroshka drops three eggs - you lose', 360, 90);
-        this.ctx.fillText('Collect as many as eggs as you can - Matroshka is immortal', 460, 150); 
+        this.ctx.fillText('Collect as many eggs as you can - Matroshka is immortal', 460, 150); 
         this.ctx.fillText('Press ‘A’ to fetch the egg on the top left', 340, 210);
         this.ctx.fillText('Press ‘Z’ to fetch the egg on the bottom left', 370, 270);
         this.ctx.fillText('Press ‘K’ to fetch the egg on the top right', 350, 330);
