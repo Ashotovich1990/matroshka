71563e4bbd5948660afa00cda7d13988908c97a9
Aram Sargsyan <aramsargsyan9902gmail.com>
2019-03-20 18:55:57 -0700

    Style main page

--- src/bundle.js
+++ src/bundle.js
@@ -841,21 +841,21 @@ class Intro {
     
     move() {
         this.ctx.fillStyle = "darkred"; 
-        this.ctx.fillRect(0,0,1000,1200);
+        this.ctx.fillRect(0,0,1000,420);
         this.ctx.font = "30px Comic Sans MS";
         this.ctx.fillStyle = "white";
         this.ctx.textAlign = "center";
-        this.ctx.fillText('RULES', 100, 150);
-        this.ctx.fillText('Matroshka drops three eggs - you lose', 320, 200);
-        this.ctx.fillText('Collect as many eggs as you can - Matroshka is immortal', 440, 250); 
-        this.ctx.fillText('Press ‘A’ to catch the egg on the top left', 340, 300);
-        this.ctx.fillText('Press ‘Z’ to catch the egg on the bottom left', 370, 350);
-        this.ctx.fillText('Press ‘K’ to catch the egg on the top right', 350, 400);
-        this.ctx.fillText('Press ‘M’ to catch the egg on the bottom right', 380, 450); 
+        this.ctx.fillText('RULES', 100, 70);
+        this.ctx.fillText('Matroshka drops three eggs - you lose', 320, 120);
+        this.ctx.fillText('Collect as many eggs as you can - Matroshka is immortal', 440, 170); 
+        this.ctx.fillText('Press ‘A’ to catch the egg on the top left', 340, 220);
+        this.ctx.fillText('Press ‘Z’ to catch the egg on the bottom left', 370, 270);
+        this.ctx.fillText('Press ‘K’ to catch the egg on the top right', 350, 320);
+        this.ctx.fillText('Press ‘M’ to catch the egg on the bottom right', 380, 370); 
         setTimeout(() => {
-            this.ctx.drawImage(this.basket, 750, 350, 80, 80)
-            this.ctx.drawImage(this.matroshka, 830, 210, 140, 250)
-        }, 40)
+            this.ctx.drawImage(this.basket, 740, 295, 80, 80)
+            this.ctx.drawImage(this.matroshka, 830, 130, 140, 250)
+        }, 50)
     }
 
 }
